6cf51f248bb75f331ccc3e20b2101cd2d7d5256a
Lulow08 <yostinomgdan08@gmail.com>
2026-06-28 19:30:37 -0500

    refactor: change entry scene

--- src/main/java/com/trifasico/x50zo/model/AppInitializer.java
+++ src/main/java/com/trifasico/x50zo/model/AppInitializer.java
@@ -1,26 +1,27 @@
 package com.trifasico.x50zo.model;
 
 import com.trifasico.x50zo.view.SceneManager;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
+/**
+ * Handles the application's bootstrapping sequence and initial stage preparation.
+ */
 public class AppInitializer {
 
-    private static final String APP_TITLE    = "50zo";
-    //private static final String FAVICON_PATH = "/icons/favicon.png";
+    private static final String APP_TITLE   = "50zo";
+    private static final String ENTRY_SCENE = "menu-view.fxml";
 
-    private static final String ENTRY_SCENE  = "game-view.fxml";
+    /**
+     * Configures the window parameters, registers the scene infrastructure, and sets the initial view.
+     *
+     * @param stage The primary stage provided by the runtime environment launcher.
+     * @throws IOException If the default entry scene template file fails to map correctly.
+     */
     public void start(Stage stage) throws IOException {
-        /*Image favicon = new Image(Objects.requireNonNull(
-                getClass().getResourceAsStream(FAVICON_PATH)
-        ));*/
-
         stage.setTitle(APP_TITLE);
         stage.setResizable(false);
-        //stage.getIcons().add(favicon);
 
         SceneManager sceneManager = SceneManager.getInstance();
         sceneManager.setMainStage(stage);
